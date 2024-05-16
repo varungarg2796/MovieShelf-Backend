30774f3e8ab96c21a862e06cd90f2f18b0a5e659
varungarg2796 <varungarg2796@hotmail.com>
2024-05-16 16:17:49 +0530

    chore: Update OMDB API integration to use Axios instance

--- src/omdb-wrapper/omdb-wrapper.service.ts
+++ src/omdb-wrapper/omdb-wrapper.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class OmdbWrapperService {
-  private readonly omdbApiKey: string = process.env.OMDB_API_KEY;
+  private readonly omdbApiKey: string;
+  private readonly http: AxiosInstance;
 
   constructor(private configService: ConfigService) {
     this.omdbApiKey = this.configService.get<string>('OMDB_API_KEY');
+    this.http = axios.create({
+      baseURL: 'http://www.omdbapi.com/',
+      timeout: 5000,
+    });
   }
+
   async searchMoviesByTitle(title: string) {
     try {
-      const url = `http://www.omdbapi.com/?apikey=${this.omdbApiKey}&t=${title}`;
-      const response = await axios.get(url, { timeout: 5000 });
+      const params = new URLSearchParams({
+        apikey: this.omdbApiKey,
+        t: title,
+      });
+
+      const response = await this.http.get(`?${params.toString()}`);
       return response.data;
     } catch (error) {
       if (error.code === 'ECONNABORTED') {
